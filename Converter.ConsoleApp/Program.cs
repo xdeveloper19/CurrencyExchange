b677eb51296410f2007883d0ac3223f1a6d630cb
Darya Yakovleva <50288548+daryalove@users.noreply.github.com>
2022-02-23 23:46:40 +0300

    V0

---
+++ Converter.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Converter.Models;
+using System;
+
+namespace Converter.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = true;
+
+            while (f)
+            {
+                Console.WriteLine("Это конвертер валют.");
+                Console.WriteLine("Введите баланс РУБ");
+
+                var userinput2 = 0;
+                Int32.TryParse(Console.ReadLine(), out userinput2);
+
+                if (userinput2 == 0)
+                    Console.WriteLine("!!!!!");
+
+                Console.WriteLine("   выберите действие \n 1 - РУБ > USD  \n 2 - РУБ > EUR \n 3 - РУБ > FRA \n 4 - ДОПОЛНИТЕЛЬНОЕ МЕНЮ \n 5- ВЫХОД НА)(УЙ");
+                string swiher = Console.ReadLine();
+                switch (swiher)
+                {
+                    case "1":
+                        Console.WriteLine("\n \n \n выбрано первое действие РУБ > USD \n" + Math.Round(userinput2 / ConverterModel.USA));
+                        break;
+                    case "2":
+                        Console.WriteLine("\n \n \n выбрано первое действие РУБ > EUR \n" + " значение округлено до целых \n " + "   " + Math.Round(userinput2 / ConverterModel.EUR));
+                        break;
+                    case "3":
+                        Console.WriteLine("\n \n \n выбрано первое действие РУБ > FRA \n" + " значение округлено до целых \n " + "   " + Math.Round(userinput2 / ConverterModel.FRA));
+                        break;
+                    case "4":
+                        Console.WriteLine("Меняем цвет");
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case "5":
+                        f = false;
+                        Console.Clear();
+                        Console.WriteLine("Говно бля!");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ Converter.Models/ConverterModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Converter.Models
+{
+    public static class ConverterModel
+    {
+        public static double EUR { get; set; } = 91.20;
+        public static double USA { get; set; } = 80.12;
+        public static double FRA { get; set; } = 145;
+
+
+    }
+}
